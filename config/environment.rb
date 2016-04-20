bcda28c067687b48db3a04285d8aba86c89a0650
9pori2 <zoheadcompany@gmail.com>
2016-04-20 09:56:35 +0900

    mu

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-IMAGE_DIR = '/home/mihara/Dropbox/mobile_sketch'
+IMAGE_DIR = '/home/mogemoge/Dropbox/mobile_sketch'
